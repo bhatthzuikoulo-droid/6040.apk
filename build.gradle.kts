28d469b01ea29963b3f7f4678c1ced6ae6cb4615
bhatthzuikoulo-droid <bhatthzuikoulo@gmail.com>
2026-06-06 10:52:24 +0700

    Add build.gradle.kts configuration

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    namespace = "com.thaihelpthai.calculator"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.thaihelpthai.calculator"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+}
